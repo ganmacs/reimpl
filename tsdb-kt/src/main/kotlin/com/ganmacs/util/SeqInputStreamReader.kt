188fea116ee00134cc1e3c7d88781f46d08ae7cb
Yuta Iwama <ganmacs@gmail.com>
2022-02-27 21:24:40 +0900

    fix bug when empty inputStream

--- tsdb-kt/src/main/kotlin/com/ganmacs/util/SeqInputStreamReader.kt
+++ tsdb-kt/src/main/kotlin/com/ganmacs/util/SeqInputStreamReader.kt
@@ -13,12 +13,18 @@ internal class SeqInputStreamReader(
     private var totalAvailable = inputStreams.fold(0) { acc, e -> acc + e.available() }
 
     override fun read(b: ByteArray, off: Int, len: Int): Int {
-        var rlen = inputStream.read(b, off, len)
+        var rlen = read0(b, off, len)
+        if (rlen == EOF) return EOF
+
         while (len > rlen) {
             inputStream = iter.nextOrNull() ?: break
-            rlen += inputStream.read(b, off + rlen, len - rlen)
-
+            val r = read0(b, off + rlen, len - rlen)
+            if (r == EOF) { // when the last inputStream in iter is empty
+                break
+            }
+            rlen += r
         }
+
         totalAvailable -= rlen
         return rlen
     }
@@ -34,9 +40,18 @@ internal class SeqInputStreamReader(
         return t
     }
 
+    private fun read0(b: ByteArray, off: Int, len: Int): Int {
+        var r = inputStream.read(b, off, len)
+        while (r == EOF) {
+            inputStream = iter.nextOrNull() ?: return EOF
+            r = inputStream.read(b, off, len)
+        }
+        return r
+    }
+
     override fun available(): Int = totalAvailable
 
     override fun close() = inputStreams.forEach { it.close() }
 }
 
-private fun <T> Iterator<out T>.nextOrNull(): T? = if (hasNext()) next() else null
+private fun <T> Iterator<T>.nextOrNull(): T? = if (hasNext()) next() else null
--- tsdb-kt/src/test/kotlin/com/ganmacs/util/SeqInputStreamReaderTest.kt
+++ tsdb-kt/src/test/kotlin/com/ganmacs/util/SeqInputStreamReaderTest.kt
@@ -39,4 +39,41 @@ internal class SeqInputStreamReaderTest {
 
         assertEquals(0, seq.available())
     }
+
+    @Test
+    fun `when hitting EOF`() {
+        val l = listOf(
+            "abcdefg".toByteArray(),
+            "hijklm".toByteArray()
+        )
+
+        val seq = SeqInputStreamReader(l.map { ll -> ByteArrayInputStream(ll) })
+        val buf = ByteArray(20)
+        var off = 0
+
+        val firstSize = l[0].size
+        off += seq.read(buf, 0, firstSize)
+        assertEquals(buf.take(firstSize), l[0].toList())
+        assertEquals(firstSize, off)
+
+        val secondSize = l[1].size
+        off += seq.read(buf, 0, secondSize) //hit EOF
+        assertEquals(buf.take(secondSize), l[1].toList())
+        assertEquals(secondSize + firstSize, off)
+    }
+
+    @Test
+    fun `when hitting EOF2`() {
+        val l = listOf(
+            "abcdefg".toByteArray(),
+            "".toByteArray(), // last is empty
+        )
+
+        val seq = SeqInputStreamReader(l.map { ll -> ByteArrayInputStream(ll) })
+        val buf = ByteArray(30)
+
+        val size = l.fold(0) { acc, bytes -> acc + bytes.size }
+        assertEquals(size, seq.read(buf, 0, size + 1))
+        assertEquals(-1, seq.read(buf, 0, size + 1))
+    }
 }
